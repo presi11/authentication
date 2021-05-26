a5c305bcaa8ac8d04c080641118c924bbcee2946
XxDirtyDannxX <33010627+SotalvaroO@users.noreply.github.com>
2021-05-26 13:03:03 -0500

    3

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,32 +4,34 @@ import config from "../config";
 import Role from "../models/Role";
 
 export const signUp = async (req, res) => {
-  const { username, email, password, role } = req.body;
+  try {
+    const { username, email, password, roles } = req.body;
 
-  const newUser = new User({
-    username,
-    email,
-    password: await User.encryptPassword(password),
-  });
+    const newUser = new User({
+      username,
+      email,
+      password: await User.encryptPassword(password),
+    });
 
-  if (req.body.roles) {
-    const foundRoles = await Role.find({ name: { $in: roles } });
-    newUser.roles = foundRoles.map((role) => role._id);
-  } else {
-    const role = await Role.findOne({ name: "user" });
-    newUser.roles = [role._id];
+    if (req.body.roles) {
+      const foundRoles = await Role.find({ name: { $in: roles } });
+      newUser.roles = foundRoles.map((role) => role._id);
+    } else {
+      const role = await Role.findOne({ name: "user" });
+      newUser.roles = [role._id];
+    }
+
+    const savedUser = await newUser.save();
+    const token = jwt.sign({ id: savedUser._id }, config.SECRET_WORD, {
+      expiresIn: 86400, // 1 dia
+    });
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
   }
 
-  const savedUser = await newUser.save();
-  const token = jwt.sign({ id: savedUser._id }, config.SECRET_WORD, {
-    expiresIn: 86400, // 1 dia
-  });
-
-  res.status(200).json({ token });
-
-  console.log(newUser);
-
-  console.log(req.body);
 };
 
 export const signIn = async (req, res) => {
